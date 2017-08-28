d3ae10eaa349684c3457ebef7b5bc0c9a9c35ebb
Nicolás Mendoza <nicolas.mendoza@yandex.com>
2017-08-28 09:09:44 -0300

    Adding more pytest fixtures

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,5 +1,5 @@
 from hydrogen.core.wheater.statistics.elements import RainGauge
-from hydrogen.core.galaxy.planet import Planet
+from hydrogen.core.galaxy.planet import Planet, ClockWise
 import pytest
 
 
@@ -13,12 +13,39 @@ def test_fixture_rain_gauge_20_30(rain_gauge_20_30):
     assert rain_gauge_20_30.level ==  20
     assert rain_gauge_20_30.day == 30
 
+@pytest.fixture()
+def clockwise_counterclock():
+    return ClockWise.COUNTERCLOCK.value
+
+
+def test_fixture_clockwise_counterclock(clockwise_counterclock):
+    assert clockwise_counterclock == 1
+
+
+@pytest.fixture()
+def clockwise_anticlock():
+    return ClockWise.ANTICLOCK.value
+
+
+def test_fixture_clockwise_anticlock(clockwise_anticlock):
+    assert clockwise_anticlock == -1
+
 
 @pytest.fixture()
 def ferengi_planet():
     """Ferengi fixture"""
     return Planet.new_ferengi()
 
-def test_fixture_ferengi_planet(ferengi_planet):
-    assert ferengi_planet.velocity == 1
-    assert ferengi_planet.sun_distancie == 500
+def test_fixture_ferengi_planet(ferengi_planet, clockwise_counterclock):
+    assert ferengi_planet._velocity == 1 * clockwise_counterclock
+    assert ferengi_planet._sun_distance == 500
+
+
+@pytest.fixture()
+def vulcano_planet():
+    """Fixture planeta vulcano"""
+    return Planet.new_vulcano()
+
+def test_fixture_vulcano_planet(vulcano_planet, clockwise_anticlock ):
+    assert vulcano_planet._velocity == 5 * clockwise_anticlock
+    assert vulcano_planet._sun_distance == 1000
